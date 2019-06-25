488803af422d57b2920d750a6de577ddb4118c89
Tamami <tamami_polk@hotmail.com>
2019-06-25 12:11:31 -0500

    navigation and login bug fixes

--- db/CREATE_LOCAL_DATABASE.sql
+++ db/CREATE_LOCAL_DATABASE.sql
@@ -8,19 +8,18 @@ CREATE TABLE Users (
   id INT NOT NULL AUTO_INCREMENT,
   username VARCHAR(50) NOT NULL,
   password VARCHAR (50) NOT NULL,
-  first VARCHAR (50) NOT NULL,
-  last VARCHAR (50) NOT NULL,
+  fullname VARCHAR (50) NOT NULL,
   email VARCHAR (50) NOT NULL,
-  confirmit VARCHAR (50) NULL,
-  phone VARCHAR (10) NULL,
+  share BOOLEAN NOT NULL,
+  story VARCHAR (255) NULL,
   createdAt DATETIME NOT NULL,
   updatedAt DATETIME NOT NULL,
   PRIMARY KEY (id)
 );
 
 
-insert into users (username, password, first, last, email, confirmit, phone, createdAt, updatedAt)
-values ('tpolk', 'password', 'Tamami', 'Polk', 'tpolk@anywhere.com', 'password', '2147701208', sysdate(), sysdate());
+insert into users (username, password, fullname, email, share, story, createdAt, updatedAt)
+values ('tpolk', 'password', 'Tamami Polk', 'tpolk@anywhere.com', 0, '2147701208', sysdate(), sysdate());
 
 
 
