3f14a12bac9cd57fe7663113ab58b009213c4706
Tamami <tamami_polk@hotmail.com>
2019-06-27 15:23:24 -0500

    Updated database configurations

--- models/index.js
+++ models/index.js
@@ -5,7 +5,9 @@ var path = require("path");
 var Sequelize = require("sequelize");
 var basename = path.basename(module.filename);
 // var env = process.env.NODE_ENV || "development";
-var env = process.env.NODE_ENV || "local";
+// var env = process.env.NODE_ENV || "local";
+var env = process.env.NODE_ENV || "production";
+
 var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
 
